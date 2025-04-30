654dad9c2cd33a37be0342f2e0050c69c58d1f55
Gajjala Vagdevi <vagdevi0826@gmail.com>
2025-04-30 23:21:53 +0530

    Add files via upload

---
+++ lazy_predict.py
@@ -0,0 +1,23 @@
+#lazy predict 
+#pip install lazypredict
+import lazypredict
+
+from lazypredict.Supervised import LazyRegressor
+from sklearn import datasets
+from sklearn.utils import shuffle
+import numpy as np
+
+diabetes  = datasets.load_diabetes()
+X, y = shuffle(diabetes.data, diabetes.target, random_state=13)
+X = X.astype(np.float32)
+
+offset = int(X.shape[0] * 0.9)
+
+X_train, y_train = X[:offset], y[:offset]
+X_test, y_test = X[offset:], y[offset:]
+
+reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+
+print(models)
+
